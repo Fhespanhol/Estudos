07ca3a4520c7a746b1bd92149d53fb58e2a4af92
Flavio Ferreira Hespanhol <fhespanhol09@gmail.com>
2022-04-11 18:25:28 -0300

    11 04 2022

---
+++ Javascript/Aula006/ifelse.js
@@ -0,0 +1,9 @@
+var vel=61
+console.log('A velocidade do seu carro é: '+ vel + 'Km/h' )
+if(vel>60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!!')
+
+}
+console.log('Dirija sempre usando o cinto de segurança')
+
+
